dbac296235952da3862814258e91deb81b7c6483
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-01-24 00:28:47 +0530

    Fix redux boilerplate

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import sampleReducer from './sampleReducer';
 
 /**
  * 
@@ -15,5 +16,5 @@ import { combineReducers } from 'redux';
  */
 
 export default combineReducers({
-
+    sample: sampleReducer
 })
---
+++ client/src/redux/reducers/sampleReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+
+}
+
+const sampleReducer = (state = initState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default sampleReducer;
